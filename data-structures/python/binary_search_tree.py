346d65279324eb8d6ae13ffd71dae9f7334ec505
Professor Ragna <jpbullalayao@gmail.com>
2023-11-06 23:29:37 -0800

    Add Binary Search Tree Iterative Solutions (#3)
    * Update BST implementation
    
    * Add inorder traversal
    
    * Add Postorder Iterative method
    
    * Remove manual insertions

--- data-structures/python/binary_search_tree.py
+++ data-structures/python/binary_search_tree.py
@@ -5,7 +5,7 @@ class BinarySearchTree:
         self.root = None
 
     # Duplicate values get inserted to right of node
-    def insert(self, node, val):
+    def insert(self, val, node = None):
         if not node:
             self.root = BSTNode(val)
             return
@@ -15,41 +15,101 @@ class BinarySearchTree:
                 node.right = BSTNode(val)
 
             else:
-                self.insert(node.right, val)
+                self.insert(val, node.right)
 
         else:
             if not node.left:
                 node.left = BSTNode(val)
 
             else:
-                self.insert(node.left, val)
+                self.insert(val, node.left)
 
 
     def print_in_order(self, node):
         if not node:
-            return
+            return None
 
         self.print_in_order(node.left)
         print(node.val)
         self.print_in_order(node.right)
+
+
+    def print_in_order_iterative(self):        
+        if not self.root:
+            return None
+
+        stack = []
+        values = []
+
+        node = self.root
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                values.append(node.val)
+                node = node.right
+
+        for value in values:
+            print(value)
 
 
     def print_preorder(self, node):
         if not node:
-            return
+            return None
         
         print(node.val)
-        self.print_in_order(node.left)
-        self.print_in_order(node.right)
+        self.print_preorder(node.left)
+        self.print_preorder(node.right)
     
+
+    def print_preorder_iterative(self):
+        if not self.root:
+            return None
+        
+        stack = [self.root]
+
+        while stack:
+            node = stack.pop()
+            print(node.val)
+
+            if node.right:
+                stack.append(node.right)
+
+            if node.left:
+                stack.append(node.left)
+
  
     def print_postorder(self, node):
         if not node:
-            return
+            return None
         
-        self.print_in_order(node.left)
-        self.print_in_order(node.right)
+        self.print_postorder(node.left)
+        self.print_postorder(node.right)
         print(node.val)
+
+
+    def print_postorder_iterative(self):
+        if not self.root:
+            return None
+        
+        stack = [self.root]
+        values = []
+
+        while stack:
+            node = stack.pop()
+
+            values = [node.val] + values
+            
+            if node.left:
+                stack.append(node.left)
+
+            if node.right:
+                stack.append(node.right)
+
+        for value in values:
+            print(value)
 
 
     def find(self, node, val):
